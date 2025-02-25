fefc64407afcd2909ce32f90604ad91dddaca40c
Breno de Barros <brenowbarros21@gmail.com>
2025-02-24 21:05:37 -0400

    [FEAT] Adicionar endpoint para busca de notícias por id

--- app/controllers/news_controller.py
+++ app/controllers/news_controller.py
@@ -1,15 +1,24 @@
 from fastapi import APIRouter, Query
 from starlette import status
+
+from app.services.get_news_service import GetNewsService
 from app.services.get_recommendation_news_by_user_id_service import GetRecommendationNewsByUserIdService
 
 router = APIRouter(prefix='/news')
 
+get_news_service = GetNewsService()
 get_recommendation_news_by_user_id_service = GetRecommendationNewsByUserIdService()
 
 
-@router.get('/recommendation/{user_id}',
-            description='Retorna uma lista de notícias recomendados para um usuário específico',
+@router.get('/{news_id}',
+            description='Busca uma notícia pelo seu ID.',
             status_code=status.HTTP_200_OK)
-def get_news(user_id: int,
-             top_k: int = Query(5, description="Número de notícias a serem retornadas (máximo de 20)")) -> list:
+def get_news(news_id: int):
+    return get_news_service.execute(news_id)
+
+@router.get('/recommendation/{user_id}',
+            description='Retorna uma lista de notícias recomendados para um usuário específico.',
+            status_code=status.HTTP_200_OK)
+def get_recommended_news(user_id: int,
+             top_k: int = Query(5, description="Número de notícias a serem retornadas (máximo de 20)")):
     return get_recommendation_news_by_user_id_service.execute(user_id, top_k)
